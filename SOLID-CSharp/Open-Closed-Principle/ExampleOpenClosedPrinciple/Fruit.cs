531fbf9ba33582ae6bd4d38eea940cbc9bab6b87
Juan Carlos Contreras <cmjcarlos7@gmail.com>
2021-02-13 23:50:47 +0100

    Wrong example.

---
+++ SOLID-CSharp/Open-Closed-Principle/ExampleOpenClosedPrinciple/Fruit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleOpenClosedPrinciple
+{
+    public class Fruit
+    {
+        private string type;
+
+        public Fruit(string type)
+        {
+            this.type = type;
+        }
+
+        public string GetFruitType()
+        {
+            return type;
+        }
+    }
+}
---
+++ SOLID-CSharp/Open-Closed-Principle/ExampleOpenClosedPrinciple/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ExampleOpenClosedPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fruit[] Fruits =
+            {
+                new Fruit("oranges"),
+                new Fruit("bananas"),
+                new Fruit("strawberrys")
+            };
+            PrintFruitsPrice(Fruits);
+        }
+
+        private static void PrintFruitsPrice(Fruit[] Fruits)
+        {
+            foreach(var fruit in Fruits)
+            {
+                if (fruit.GetFruitType() == "oranges") Console.WriteLine(1.2);
+                if (fruit.GetFruitType() == "bananas") Console.WriteLine(0.85);
+                if (fruit.GetFruitType() == "strawberrys") Console.WriteLine(2);
+            }
+        }
+    }
+}
