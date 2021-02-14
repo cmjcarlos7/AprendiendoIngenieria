15fccdc70a5c0136d30b57e7f7659bd0e209ac32
Juan Carlos Contreras <cmjcarlos7@gmail.com>
2021-02-14 12:12:49 +0100

    Wrong Interface Segmentation principle.

---
+++ SOLID-CSharp/Interface-Segregation-interface/ExampleInterfaceSegregationPrinciple/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ExampleInterfaceSegregationPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Swimmer swimmer = new Swimmer();
+            swimmer.Run(5);
+        }
+    }
+
+    public interface IAthlete
+    {
+        void WarmUp(int time);
+        void Run(int time);
+        void Swing(int time);
+    }
+
+    class Swimmer : IAthlete
+    {
+        public void Run(int time)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Swing(int time)
+        {
+            // Proccess to Swing ...
+        }
+
+        public void WarmUp(int time)
+        {
+            // Proccess to WarmUp ...
+        }
+    }
+
+}
