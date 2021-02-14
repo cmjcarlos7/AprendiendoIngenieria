0a05b0d8dfac1322898aa7efe6cb9e6821e987df
Juan Carlos Contreras <cmjcarlos7@gmail.com>
2021-02-14 20:53:18 +0100

    Wrong Dependency Inversion Principle.

---
+++ SOLID-CSharp/Dependency Inversion Principle/DependencyInversionPrinciple/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DependencyInversionPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    class MySQLDataBaseService
+    {
+        public void GetData()
+        {
+            // Get data from database.
+        }
+    }
+
+    class DataAccess
+    {
+        private MySQLDataBaseService MySQLDataBaseService;
+
+        public DataAccess()
+        {
+            MySQLDataBaseService = new MySQLDataBaseService();
+        }
+
+        public void GetData()
+        {
+            MySQLDataBaseService.GetData();
+            // ...
+        }
+    }
+}
